f796f60a38a2f7456b6902bda2642549a761836c
user <user@lab1554.tumo.lab>
2017-09-27 18:29:52 +0400

    add new scraper

--- scraper.js
+++ scraper.js
@@ -11,14 +11,31 @@ scrapeIt("https://www.menu.am/restaurant/type.html", {
             name: ".list-title > a",
             url: {
                 selector: "a"
-              , attr: "href"
+                , attr: "href"
             },
-            tag: ".list-title > span"
+            tag: ".list-title > span",
+            rating: {
+                selector: ".list-rate div>.fl",
+                attr: "style",
+                convert: function (x) {
+                    var re = /width: (\d*\.?\d*)/i;
+                    if (re.test(x)) {
+                        var found = x.match(re);
+                        console.log(found[1]);
+                        return found[1];
+                    } else {
+                        return "";
+                    }
+                }
+            }
         }
     }
+
+
+
 }).then(function (page) {
-        console.log(page);
-        jsonfile.writeFile(file, page, { spaces: 2 }, function (err) {
-            console.error(err);
-        });
+    console.log(page);
+    jsonfile.writeFile(file, page, { spaces: 2 }, function (err) {
+        console.error(err);
     });
+});
